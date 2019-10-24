29a47db8923f827d3d60fa21b45a638336677faf
Sebastien LABELLE <slabelle@student.42.fr>
2019-10-24 21:42:50 +0200

    Update 42

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -14,16 +14,19 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n < 0)
+	long	long_n;
+
+	long_n = n;
+	if (long_n < 0)
 	{
 		ft_putchar_fd('-', fd);
-		n = -n;
+		long_n = -long_n;
 	}
-	if (n > 9)
+	if (long_n > 9)
 	{
-		ft_putnbr_fd(n / 10, fd);
-		ft_putnbr_fd(n % 10, fd);
+		ft_putnbr_fd(long_n / 10, fd);
+		ft_putnbr_fd(long_n % 10, fd);
 	}
 	else
-		ft_putchar_fd(n + '0', fd);
+		ft_putchar_fd(long_n + '0', fd);
 }
