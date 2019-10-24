1917b98a9d07e1035f720bd68212168c65d9ca9b
Sebastien LABELLE <slabelle@student.42.fr>
2019-10-24 22:27:21 +0200

    Update 42

--- ft_substr.c
+++ ft_substr.c
@@ -20,10 +20,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (!(sub = malloc(sizeof(char) * (len + 1))))
 		return (NULL);
 	i = 0;
-	while (s[i + start] && i < len)
+	if (!(start > ft_strlen(s)))
 	{
-		sub[i] = s[i + start];
-		i++;
+		while (s[start] && i < len)
+			sub[i++] = s[start++];
 	}
 	sub[i] = '\0';
 	return (sub);
