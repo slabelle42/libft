462e9b31778cf25153715a69f0fee617e6e67e60
Sebastien LABELLE <slabelle@student.42.fr>
2019-11-01 15:35:24 +0100

    Update 42

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: slabelle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/12 15:52:01 by slabelle          #+#    #+#             */
-/*   Updated: 2019/11/01 15:21:45 by slabelle         ###   ########.fr       */
+/*   Updated: 2019/11/01 15:34:45 by slabelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,16 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 
 	dst_len = ft_strlen(dst);
 	src_len = ft_strlen(src);
-	if (size < dst_len)
-		return (size + src_len);
-	i = 0;
-	while (src[i] && (dst_len + i) < (size - 1))
+	if (size >= dst_len)
 	{
-		dst[dst_len + i] = src[i];
-		i++;
+		i = 0;
+		while (src[i] && (size - 1) > (dst_len + i))
+		{
+			dst[dst_len + i] = src[i];
+			i++;
+		}
+		dst[dst_len + i] = '\0';
+		return (dst_len + src_len);
 	}
-	dst[dst_len + i] = '\0';
-	return (dst_len + src_len);
+	return (size + src_len);
 }
