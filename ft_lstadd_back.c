8ccfbe053383d81b90341be019e7abf8d5ae4857
Sebastien LABELLE <slabelle@student.42.fr>
2019-11-11 18:25:49 +0100

    Update 42

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: slabelle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/24 15:44:32 by slabelle          #+#    #+#             */
-/*   Updated: 2019/11/01 20:22:23 by slabelle         ###   ########.fr       */
+/*   Updated: 2019/11/11 18:25:28 by slabelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,16 @@ void	ft_lstadd_back(t_list **alst, t_list *new)
 {
 	t_list	*tmp;
 
-	if (alst && *alst && new)
+	if (alst && new)
 	{
-		tmp = *alst;
-		while (tmp->next)
-			tmp = tmp->next;
-		tmp->next = new;
+		if (!(*alst))
+			*alst = new;
+		else
+		{
+			tmp = *alst;
+			while (tmp->next)
+				tmp = tmp->next;
+			tmp->next = new;
+		}
 	}
 }
