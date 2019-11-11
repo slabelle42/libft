15231c19e452bcdc6fcb3193d86c32c2c1afdcaa
Sebastien LABELLE <slabelle@student.42.fr>
2019-11-11 18:19:11 +0100

    Update 42

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: slabelle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/24 15:44:16 by slabelle          #+#    #+#             */
-/*   Updated: 2019/11/01 20:22:01 by slabelle         ###   ########.fr       */
+/*   Updated: 2019/11/11 18:18:55 by slabelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_lstadd_front(t_list **alst, t_list *new)
 {
-	if (alst && *alst && new)
+	if (alst && new)
 	{
 		new->next = *alst;
 		*alst = new;
