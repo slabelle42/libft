e2bd7fbcc372c4e6977d106aa2d36112b08fcf63
Sebastien LABELLE <slabelle@student.42.fr>
2019-11-11 18:41:00 +0100

    Update 42

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: slabelle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/24 15:49:17 by slabelle          #+#    #+#             */
-/*   Updated: 2019/11/01 20:22:49 by slabelle         ###   ########.fr       */
+/*   Updated: 2019/11/11 18:40:19 by slabelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 	t_list	*tmp1;
 	t_list	*tmp2;
 
-	if (lst && *lst && del)
+	if (lst && del)
 	{
 		tmp1 = *lst;
 		while (tmp1)
@@ -26,7 +26,6 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 			tmp1 = tmp1->next;
 			del(tmp2->content);
 			free(tmp2);
-			tmp2 = NULL;
 		}
 		*lst = NULL;
 	}
