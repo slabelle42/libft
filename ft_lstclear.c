f48acd2a4aae3fc242258dbb17902110f6b5fca3
slabelle42 <56270385+slabelle42@users.noreply.github.com>
2020-07-24 14:13:28 +0200

    Update ft_lstclear.c

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -21,8 +21,7 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 		while (lst && *lst)
 		{
 			tmp = (*lst)->next;
-			del(lst->content);
-			free(lst);
+			ft_lstdelone(lst, del);
 			*lst = tmp;
 		}
 	}
